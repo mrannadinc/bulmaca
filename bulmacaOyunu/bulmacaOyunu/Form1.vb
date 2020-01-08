744c54792c356e49feef1936d807fe84598b5fde
H.İbrahim Annadinç <44865773+mrannadinc@users.noreply.github.com>
2020-01-08 19:39:00 +0300

    Add files via upload

---
+++ bulmacaOyunu/bulmacaOyunu/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '24
+        cblev1(Button1, Button2)
+        cblev1(Button1, Button4)
+        fnLev1()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '135
+        cblev1(Button2, Button1)
+        cblev1(Button2, Button3)
+        cblev1(Button2, Button5)
+        fnLev1()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        '26
+        cblev1(Button3, Button2)
+        cblev1(Button3, Button6)
+        fnLev1()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        '157
+        cblev1(Button4, Button1)
+        cblev1(Button4, Button5)
+        cblev1(Button4, Button7)
+        fnLev1()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        '2468
+        cblev1(Button5, Button2)
+        cblev1(Button5, Button4)
+        cblev1(Button5, Button6)
+        cblev1(Button5, Button8)
+        fnLev1()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        '359
+        cblev1(Button6, Button3)
+        cblev1(Button6, Button5)
+        cblev1(Button6, Button9)
+        fnLev1()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        '48
+        cblev1(Button7, Button4)
+        cblev1(Button7, Button8)
+        fnLev1()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        '759
+        cblev1(Button8, Button7)
+        cblev1(Button8, Button5)
+        cblev1(Button8, Button9)
+        fnLev1()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        '68
+        cblev1(Button9, Button6)
+        cblev1(Button9, Button8)
+        fnLev1()
+    End Sub
+End Class
---
+++ bulmacaOyunu/bulmacaOyunu/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+    Sub cblev1(ByRef button1 As Button, ByRef button2 As Button)
+        If button2.Text = "" Then
+            button2.Text = button1.Text
+            button1.Text = ""
+        End If
+    End Sub
+    Sub fnLev1()
+        If Form1.Button1.Text = "1" And Form1.Button2.Text = "2" And Form1.Button3.Text = "3" And Form1.Button4.Text = "4" And
+            Form1.Button5.Text = "5" And Form1.Button6.Text = "6" And Form1.Button7.Text = "7" And Form1.Button8.Text = "8" And
+            Form1.Button9.Text = "" Then
+            MsgBox("Congratulations!You succeeded")
+        End If
+    End Sub
+End Module
